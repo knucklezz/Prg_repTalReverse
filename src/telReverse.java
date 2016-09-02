6eaf6d91dfd7a460e71b382fa593163f41bd4d12
luni14 <te14.niklas.lundstrom@umea.nti.se>
2016-09-02 11:35:56 +0200

    Initial commit

---
+++ src/telReverse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by luni14 on 2016-09-02.
+ */
+public class talOmvändare{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt;
+
+
+    }
+}
