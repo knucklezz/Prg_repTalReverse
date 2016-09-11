4e9e04f6e57091251dff7ee18d93c2f104fefced
Niklas Lundström <te14.niklas.lundstrom@umea.nti.se>
2016-09-11 19:23:32 +0200

    test

--- src/telReverse.java
+++ src/telReverse.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 /**
  * Created by luni14 on 2016-09-02.
  */
-public class talOmvändare{
+public class telReverse {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt;
+        int tal = scanner.nextInt();
 
-
+        System.out.println("tal");
     }
 }
